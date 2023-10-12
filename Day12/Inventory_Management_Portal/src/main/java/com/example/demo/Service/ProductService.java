7b2c12733881ed7fdbf8ba01f1d0c89d6e46508c
MaheshBoopathiAP <727821tuit055@gmail.com>
2023-10-12 07:33:37 +0530

    main

---
+++ Day12/Inventory_Management_Portal/src/main/java/com/example/demo/Service/ProductService.java
@@ -0,0 +1,47 @@
+package com.example.demo.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Model.Product;
+import com.example.demo.Repository.ProductRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        Optional<Product> productOptional = productRepository.findById(id);
+        return productOptional.orElse(null);
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product updatedProduct) {
+        Optional<Product> productOptional = productRepository.findById(id);
+        if (productOptional.isPresent()) {
+            updatedProduct.setId(id);
+            return productRepository.save(updatedProduct);
+        }
+        return null; // Product not found
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
