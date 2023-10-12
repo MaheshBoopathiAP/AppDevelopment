79eb6ffd2dd67b1f7ffa9fe659595f7bc45bfa54
MaheshBoopathiAP <727821tuit055@gmail.com>
2023-10-12 07:27:08 +0530

    main

---
+++ Day9/src/components/SidebarData.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+import { FiPlusCircle } from 'react-icons/fi';
+import { FaEdit } from 'react-icons/fa';
+import { FiGrid } from 'react-icons/fi';
+
+
+export const SidebarData = [
+  {
+    title: "Dashboard",
+    path: "/dashboard",
+    icon: <FiGrid />,
+    cName: "nav-text",
+    
+
+  },
+  {
+    title: "Add Product",
+    path: "/addproduct",
+    icon:  <FiPlusCircle /> ,
+    cName: "nav-text",
+  },
+  {
+    title: "Edit Product",
+    path: "/editproduct",
+    icon:   <FaEdit />,
+    cName: "nav-text",
+  },
+  {
+    title: "List all Products",
+    path: "/products",
+    icon: <IoIcons.IoIosPaper />,
+    cName: "nav-text",
+    
+  },
+ 
+ 
+  {
+    title: "Support",
+    path: "/contact",
+    icon: <IoIcons.IoMdHelpCircle />,
+    cName: "nav-text",
+   
+  },
+];
